30992e83254f1a0ec5c669bf080b4858dbf23120
mustapha335 <reezy77jay@gmail.com>
2022-02-02 03:16:13 +0000

    testing

--- food_desert/src/App.js
+++ food_desert/src/App.js
@@ -1,13 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Muiz like shit weh weh inside code</header>
       <h2>tada!!</h2>
+      <h2>tada!!</h2>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

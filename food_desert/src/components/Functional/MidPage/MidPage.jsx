0a35f189c15df5b5d976cc2019bb6ba6c9060f81
mustapha335 <reezy77jay@gmail.com>
2022-02-03 17:05:14 +0000

    background implemented

--- food_desert/src/components/Functional/MidPage/MidPage.jsx
+++ food_desert/src/components/Functional/MidPage/MidPage.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import MidBg from '../midBg/MidBg'
+import './midPage.css'
+import bg from '../../../Resources/images/food1.jpeg'
 
 const MidPage = () => {
   return (
-    <div>
-      <h1>Hello!!</h1>
+    <div
+      class="h-96 bg-no-repeat bg-cover bg-center"
+      style={{ backgroundImage: `url(${bg})` }}
+    >
+      <MidBg />
     </div>
   )
 }
---
+++ food_desert/src/components/Functional/midBg/MidBg.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const MidBg = () => {
+  return <h1 class="h-100 w-100">Hello world!</h1>
+}
+
+export default MidBg

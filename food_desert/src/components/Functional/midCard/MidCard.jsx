3988c126dba846c1a8129747cda003260dd030d7
mustapha335 <reezy77jay@gmail.com>
2022-02-05 00:36:23 +0000

    mid Card

--- food_desert/src/components/Functional/midCard/MidCard.jsx
+++ food_desert/src/components/Functional/midCard/MidCard.jsx
@@ -4,7 +4,8 @@ const MidCard = () => {
   return (
     <div>
       <div class="w-80 h-80 mx-24 absolute mt-5 bg-slate-100 rounded-xl p-8 dark:bg-slate-800/40 ">
-        <h2 class="text-white">hello world</h2>
+        <h2 class="text-white text-center">hello world</h2>
+        <hr />
       </div>
     </div>
   )

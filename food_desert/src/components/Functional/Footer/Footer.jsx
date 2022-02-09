415ed13532ef2e61c6cf7c92fa5f8c036607109d
mustapha335 <reezy77jay@gmail.com>
2022-02-09 03:14:26 +0000

    footer implimented

--- food_desert/src/components/Functional/Footer/Footer.jsx
+++ food_desert/src/components/Functional/Footer/Footer.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
+import './footer.css'
 
 const Footer = () => {
   return (
-    <div className="flex ">
+    <div className="container">
       <div className="">
         <img
           src="https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
           alt="food"
-          className="h-auto w-max "
+          className="img"
         />
       </div>
-      <div></div>
+      <div>
+        <form>
+          <input type="text" className="title_input" placeholder="title.." />
+
+          <textarea className="title_input" rows="10" cols="50" />
+        </form>
+      </div>
     </div>
   )
 }
